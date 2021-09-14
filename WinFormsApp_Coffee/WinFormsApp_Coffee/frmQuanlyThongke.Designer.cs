98f0cfa9949a24e722ae5e1b018af2036aafbcd8
TrungHieuPh <hieuphan112000@gmail.com>
2021-09-14 14:25:32 +0700

    Hieu add file

---
+++ WinFormsApp_Coffee/WinFormsApp_Coffee/frmQuanlyThongke.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace WinFormsApp_Coffee
+{
+    partial class FrmThongke
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dgvQuanlythongke = new System.Windows.Forms.DataGridView();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.btnthongke = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbbtennhanvien = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbbtendouong = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvQuanlythongke)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dgvQuanlythongke);
+            this.panel1.Location = new System.Drawing.Point(12, 156);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(858, 430);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.cbbtendouong);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.cbbtennhanvien);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.btnthongke);
+            this.panel2.Controls.Add(this.dateTimePicker2);
+            this.panel2.Controls.Add(this.dateTimePicker1);
+            this.panel2.Location = new System.Drawing.Point(12, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(861, 138);
+            this.panel2.TabIndex = 1;
+            // 
+            // dgvQuanlythongke
+            // 
+            this.dgvQuanlythongke.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvQuanlythongke.Location = new System.Drawing.Point(3, 0);
+            this.dgvQuanlythongke.Name = "dgvQuanlythongke";
+            this.dgvQuanlythongke.RowHeadersWidth = 51;
+            this.dgvQuanlythongke.RowTemplate.Height = 29;
+            this.dgvQuanlythongke.Size = new System.Drawing.Size(852, 427);
+            this.dgvQuanlythongke.TabIndex = 0;
+            this.dgvQuanlythongke.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(58, 11);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(250, 27);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(426, 11);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(241, 27);
+            this.dateTimePicker2.TabIndex = 1;
+            // 
+            // btnthongke
+            // 
+            this.btnthongke.Location = new System.Drawing.Point(723, 11);
+            this.btnthongke.Name = "btnthongke";
+            this.btnthongke.Size = new System.Drawing.Size(110, 44);
+            this.btnthongke.TabIndex = 2;
+            this.btnthongke.Text = "Thống kê";
+            this.btnthongke.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Từ";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(344, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Đến";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Tên nhân viên";
+            // 
+            // cbbtennhanvien
+            // 
+            this.cbbtennhanvien.FormattingEnabled = true;
+            this.cbbtennhanvien.Location = new System.Drawing.Point(153, 59);
+            this.cbbtennhanvien.Name = "cbbtennhanvien";
+            this.cbbtennhanvien.Size = new System.Drawing.Size(155, 28);
+            this.cbbtennhanvien.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(344, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(161, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Tên danh mục đồ uống";
+            // 
+            // cbbtendouong
+            // 
+            this.cbbtendouong.FormattingEnabled = true;
+            this.cbbtendouong.Location = new System.Drawing.Point(517, 67);
+            this.cbbtendouong.Name = "cbbtendouong";
+            this.cbbtendouong.Size = new System.Drawing.Size(150, 28);
+            this.cbbtendouong.TabIndex = 8;
+            this.cbbtendouong.SelectedIndexChanged += new System.EventHandler(this.cbbtendouong_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(723, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 49);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Thống kê đồ uống bán chạy";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // FrmThongke
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(885, 598);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmThongke";
+            this.Text = "Quản lý thống kê";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvQuanlythongke)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dgvQuanlythongke;
+        private System.Windows.Forms.Button btnthongke;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbbtendouong;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbbtennhanvien;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ WinFormsApp_Coffee/WinFormsApp_Coffee/frmQuanlythongke.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormsApp_Coffee
+{
+    public partial class FrmThongke : Form
+    {
+        public FrmThongke()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbbtendouong_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
