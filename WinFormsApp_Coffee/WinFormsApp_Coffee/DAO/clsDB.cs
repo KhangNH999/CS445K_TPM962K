9f865316b7e41d8e1c7c67d10c6cb214424eff52
NguyenDan888 <86759507+NguyenDan888@users.noreply.github.com>
2021-09-15 09:18:50 +0700

    Dan da gui

---
+++ WinFormsApp_Coffee/WinFormsApp_Coffee/DAO/clsDB.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp_Coffee.DAO
+{
+    class clsDB
+    {
+        //Tạo kết nối
+        private string conSTR = @"Data Source=.\SQLEXPRESS;Initial Catalog=Quanlyquancoffee;Integrated Security=True";
+        //Phương thức này dùng để thực thi câu truy vấn
+        public DataTable execQuery(string query, object[] parameter = null)
+        {
+            DataTable data = new DataTable();
+
+            using (SqlConnection con = new SqlConnection(conSTR))
+            {
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                if (parameter != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            cmd.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+
+                SqlDataAdapter adp = new SqlDataAdapter(cmd);
+
+                adp.Fill(data);
+
+                con.Close();
+            }
+
+            return data;
+        }
+        //Phương thức này trả về số hàng được thực hiện bởi truy vấn
+        public int execNonQuery(string query, object[] parameter = null)
+        {
+            int data = 0;
+
+            using (SqlConnection con = new SqlConnection(conSTR))
+            {
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                if (parameter != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            cmd.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+
+                data = cmd.ExecuteNonQuery();
+
+                con.Close();
+            }
+
+            return data;
+        }
+        //Phương thức này trả về giá trị hàng đầu tiên và cột đầu tiên của kết quả truy vấn.
+        public object execScalar(string query, object[] parameter = null)
+        {
+            object data = 0;
+
+            using (SqlConnection con = new SqlConnection(conSTR))
+            {
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                if (parameter != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            cmd.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+
+                data = cmd.ExecuteScalar();
+
+                con.Close();
+            }
+
+            return data;
+        }
+    }
+}
